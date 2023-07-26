99149d9cd52a3094168ce71acf30c35582578103
Haroon Asif <haroon.asif@tkxel.com>
2023-07-26 14:48:22 +0500

    implemented  splash screen for both ios and android

--- src/main.js
+++ src/main.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import IntroCreateAccount from './screens/IntroCreateAccount';
-import CreateAccount from './screens/CreateAccount';
+import IntroCreateAccount from "./screens/IntroCreateAccount";
+import CreateAccount from "./screens/CreateAccount";
 
-import { ScreenNames } from './screens/ScreenNames';
+import { ScreenNames } from "./screens/ScreenNames";
+import SplashScreen from "react-native-splash-screen";
 
-const ScreenOptions = { headerShown: false }
+const ScreenOptions = { headerShown: false };
 
 const Stack = createStackNavigator();
 const MainStack = () => {
-	return (
-		<Stack.Navigator screenOptions={ScreenOptions}>
-			<Stack.Screen name={ScreenNames.IntroCreateAccount} component={IntroCreateAccount} />
-			<Stack.Screen name={ScreenNames.CreateAccount} component={CreateAccount} />
-		</Stack.Navigator>
-	)
-}
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 3000);
+  }, []);
+
+  return (
+    <Stack.Navigator screenOptions={ScreenOptions}>
+      <Stack.Screen
+        name={ScreenNames.IntroCreateAccount}
+        component={IntroCreateAccount}
+      />
+      <Stack.Screen
+        name={ScreenNames.CreateAccount}
+        component={CreateAccount}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const Main = () => {
-	return (
-		<NavigationContainer>
-			<MainStack />
-		</NavigationContainer>
-	)
-}
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  );
+};
 
 export default Main;
